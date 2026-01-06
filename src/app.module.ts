514a00d61e7c13c7032d085e4e6ebb104019559b
Leandro Gonzalez <leanlag@gmail.com>
2026-01-06 14:51:57 -0300

    Agregar comentario descriptivo al módulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,5 @@
+//Este es el modulo proncipal que puede contener otros modulos
+
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { ProjectsModule } from './projects/projects.module';
