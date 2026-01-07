55408424398d94361e70064f273bdcd11ef4c7a5
Leandro Gonzalez <leanlag@gmail.com>
2026-01-07 20:44:06 -0300

    Agregar implementación de AuthMiddleware y LoggerMiddleware en el módulo de proyectos

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,9 +1,24 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
+import { LoggerMiddleware } from './logger/logger.middleware';
 
 @Module({
   controllers: [ProjectsController],
-  providers: [ProjectsService]
+  providers: [ProjectsService],
 })
-export class ProjectsModule {}
+
+/**
+ * Lo que hacemos aqui es implementar el middleware para que todas las rutas de este modulo pasen por el middleware
+ * Se puede aplicar un middleware de forma global en main.ts o de forma local en el modulo como en este caso
+ * De la forma que lo estoy aplicando es para todos las rutas "projects" pero se puede especificar las rutas de la sigueinte forma:
+ * consumer.apply(LoggerMiddleware).forRoutes(
+ * {path: 'projects', method: RequestMethod.GET}
+ * );,
+ * Le estamos diciendo que solo va a funcionar para las rutas GET de "projects"
+ */
+export class ProjectsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('projects'); //Aqui van los middlewares que queramos aplicar
+  }
+}
