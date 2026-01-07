8b55696fd9dae1b4e5cd085941f512ef3893fb10
Leandro Gonzalez <leanlag@gmail.com>
2026-01-07 16:08:53 -0300

    Agregar validaciones de longitud mínima en CreateProjectDto

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,11 +1,14 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateProjectDto {
   //Con este decorador le decimos que este campo no puede estar vacio
   @IsNotEmpty()
+  @MinLength(1) //Le decimos que el minimo de caracteres es 1
   id: number;
   @IsString()
+  @MinLength(1)
   name: string;
   @IsString()
+  @MinLength(1)
   description: string;
 }
