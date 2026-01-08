2a56060350888eb69efea5bae55e6b77d2afccfa
Leandro Gonzalez <leanlag@gmail.com>
2026-01-07 21:09:04 -0300

    Agregar módulo de pagos con controlador, servicio y DTOs para la gestión de pagos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { ProjectsModule } from './projects/projects.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [TasksModule, ProjectsModule],
+  imports: [TasksModule, ProjectsModule, PaymentsModule],
 })
 export class AppModule {}
