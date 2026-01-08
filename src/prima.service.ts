488f5ed1563e0389df3d249a2140acae50920201
Leandro Gonzalez <leanlag@gmail.com>
2026-01-07 22:01:05 -0300

    Agregar modelo User en el esquema de Prisma y crear archivo de servicio Prisma

---
+++ src/prima.service.ts
@@ -0,0 +1,13 @@
+//Aqui nos vamos a conectar con la base de datos mediante Prisma
+
+// import { Injectable } from '@nestjs/common';
+// import { PrismaClient } from './generated/prisma/client';
+// import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+
+// @Injectable()
+// export class PrismaService extends PrismaClient {
+//   constructor() {
+//     const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
+//     super({ adapter });
+//   }
+// }
